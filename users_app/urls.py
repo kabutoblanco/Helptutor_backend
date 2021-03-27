8a353b23c0462654189e31e12e7f0b8c09ff5e4b
mdquilindo <mdquilindo@unicauca.edu.co>
2021-03-27 00:49:16 -0500

    login and authenticate ok por google

--- users_app/urls.py
+++ users_app/urls.py
@@ -15,6 +15,7 @@ urlpatterns = [
     path('api/auth', include('knox.urls')),
     path('api/auth/user', UserAPI.as_view()),
     path('api/auth/login', LoginAPI.as_view()),
+    path('api/auth/google/login', LoginGoogleAPI.as_view()),
     path('api/auth/logout', knox_views.LogoutView.as_view(), name='knox_logout'),
     path('api/', include(router.urls)),
 ]
